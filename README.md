f3f97f59195004eef7649d75c6ad5a64600d9fa1
Jimmy <150027133+Jimmxyz@users.noreply.github.com>
2025-08-16 16:03:18 +0200

    v0.1 - Visual improvement - Mine and num added

--- README.md
+++ README.md
@@ -1,35 +1,44 @@
 # Minesweeper-CLI
-A multiplatform minesweeper in the CLI
+A simple minesweeper in the CLI :
+
+- **Color Support** : number, flags, score, cursor and undiscovered area are colored.
+- **Easy to use** : every keys are show and the control are intuitive.
+- **Customisable** : you can change the size of the grid and the percentage of mine.
+
+You can also try the GUI version (web) : <a href='https://jimmxyz.github.io/Simple-Minesweeper/'>https://jimmxyz.github.io/Simple-Minesweeper/</a> or see the <a href='https://github.com/Jimmxyz/Simple-Minesweeper'>repository</a>.
 
 # Installation
 First download the zip file and then unzip it.
 >[!CAUTION]
->You also need to have java installed
+>You need a cli which support RGB ANSI color (if you have a modern cli it will be supported)
+
+>[!CAUTION]
+>You also need to have java installed.
 
 >[!TIP]
->On debian and ubuntu based distribution java will be automaticly installed at the first launch
+>On debian and ubuntu based distribution java will be automaticly installed at the first launch.
 # Run it
 ## Linux :
 Change the terminal locaion to be in the folder.
-Then use this command : `bash ./launch.sh`
+Then use this command : `bash ./launch.sh`.
 ## Mac OS :
 >[!WARNING]
->The program was not tested on macos
+>The program was not tested on macos.
 
 Change the terminal locaion to be in the folder.
-Then use this command : `bash ./launch.sh`
+Then use this command : `bash ./launch.sh`.
 
 ## Windows :
 >[!WARNING]
->This cli program don't work on windows cmd or powershell
+>This cli program don't work on windows cmd or powershell.
 ### Install with WSL
-1. On your powershell type `wsl --install`
+1. On your powershell type `wsl --install`.
 
     By default the distribution will be ubuntu wich is a debian based so you don't have to change it.
    
-3. Run wsl on your powershell by type `wsl.exe`
+3. Run wsl on your powershell by type `wsl.exe`.
 
-4. Follow the installation instruction for linux
+4. Follow the installation instruction for linux.
 
 # Credit
 Created by @Jimmxyz on github
--- main.kt
+++ main.kt
@@ -15,13 +15,17 @@ fun main() {
     val width = width_input.toIntOrNull() ?: 10
     val height = height_input.toIntOrNull() ?: 10
     val grid = gen_grid(height, width, 20)
+    val cursor_x = 0
+    val cursor_y = 0
+    grid_readdy(height, width)
     val visual_grid = visual_grid_gen(height, width)
-    print_grid(visual_grid,height,width)
+    print_grid(visual_grid,height,width,cursor_x,cursor_y)
     //blocking touch
     Runtime.getRuntime().exec(arrayOf("sh","-c","stty raw -echo < /dev/tty")).waitFor()
-    loop_main()
+    loop_main(width, height, cursor_x, cursor_y, grid, visual_grid)
     Runtime.getRuntime().exec(arrayOf("sh","-c","stty sane < /dev/tty")).waitFor()
-    println()
+    print("\u001b[2K\r")
+    println("Thank you for playing Minesweeper CLI!")
 }
 
 fun gen_grid(cols: Int, rows: Int, luck:Int): Array<Array<Int>> {
@@ -32,22 +36,93 @@ fun visual_grid_gen(rows: Int, cols: Int): Array<Array<Int>> {
     return Array(rows) { Array(cols) { 0 } }
 }
 
-fun print_grid(grid_to_display: Array<Array<Int>>,height: Int = 9, width: Int = 9) {
+fun print_grid(grid_to_display: Array<Array<Int>>,height: Int = 9, width: Int = 9, cursor_i: Int = 0, cursor_j: Int = 0) {
     //Grid size 10 x 10 (test)
-    for (i in 0..height -1) {
-        for (j in 0..width - 1) {
-            if (grid_to_display[i][j] == 0) {
-                print(" - ")
+    for (i in 0..height - 1) {
+        print("\u001b[1A")
+    }
+    for (i in 0 until height) {
+        print("\u001b[2K\r")
+        for (j in 0 until width) {
+            //Bck color
+            var bck_color = "none"
+            if(grid_to_display[i][j] == 0) {
+                if((i % 2 == 0 && j % 2 == 0) || (i % 2 != 0 && j % 2 != 0)) {
+                    bck_color = "\u001b[48;2;88;168;88m"
+                } else {
+                    bck_color = "\u001b[48;2;82;153;82m"
+                }
             } else {
-                print(" ${grid_to_display[i][j]} ")
+                if((i % 2 == 0 && j % 2 == 0) || (i % 2 != 0 && j % 2 != 0)) {
+                    bck_color = "\u001b[48;2;231;164;133m"
+                } else {
+                    bck_color = "\u001b[48;2;214;140;105m"
+                }
+            }
+            // Cursor color
+            var cursor_color = "\u001b[38;2;255;255;255m"
+            
+            // Font color
+            var font_color = "none"
+            if (grid_to_display[i][j] == 1) {
+                font_color = "\u001b[38;2;73;74;210m"
+            } 
+            else if (grid_to_display[i][j] == 2) {
+                font_color = "\u001b[38;2;64;120;67m"
+            } 
+            else if (grid_to_display[i][j] == 3) {
+                font_color = "\u001b[38;2;210;73;73m"
+            } 
+            else if (grid_to_display[i][j] == 4) {
+                font_color = "\u001b[38;2;140;57;161m"
+            } 
+            else if (grid_to_display[i][j] == 5) {
+                font_color = "\u001b[38;2;243;237;38m"
+            } 
+            else if (grid_to_display[i][j] == 6) {
+                font_color = "\u001b[38;2;106;232;226m"
+            } 
+            else if (grid_to_display[i][j] == 7) {
+                font_color = "\u001b[38;2;133;133;131m"
+            } 
+            else if (grid_to_display[i][j] == 10) {
+                font_color = "\u001b[38;2;255;0;0m"
+            } 
+            else{
+                font_color = "\u001b[38;2;0;0;0m"
+            }
+            // Print the cell
+            if(i == cursor_i && j == cursor_j && grid_to_display[i][j] != 0 && grid_to_display[i][j] != 9) {
+                print("\u001B[1m"+ bck_color + cursor_color + "[" +  font_color  + grid_to_display[i][j] + "\u001b[0m" + cursor_color + bck_color + "\u001B[1m]\u001b[0m")
+            } else if(i == cursor_i && j == cursor_j && (grid_to_display[i][j] == 0 || grid_to_display[i][j] == 9)) {
+                print("\u001B[1m"+bck_color + cursor_color + "[ " + font_color + "\u001b[0m" + cursor_color + bck_color + "\u001B[1m]\u001b[0m")
+            } 
+            else if (grid_to_display[i][j] == 9 || grid_to_display[i][j] == 0) {
+                print("\u001B[1m"+bck_color + font_color + "   " + "\u001b[0m")
+            }
+            else {
+                print("\u001B[1m"+bck_color + font_color + " " + grid_to_display[i][j] + " " + "\u001b[0m")
             }
         }
         println()
     }
+    print("\u001b[2K\r")
     println("Press Q to quit, arrow to move, enter to select, and space to mark a mine.")
 }
 
-fun loop_main(){
+fun grid_readdy(height: Int, width: Int) {
+    for (i in 0..height -1) {
+        for (j in 0..width - 1) {
+            print("---")
+        }
+        println()
+    }
+}
+
+fun loop_main(width: Int, height: Int, cursor_x_imported: Int, cursor_y_imported: Int, grid: Array<Array<Int>>, visual_grid_imported: Array<Array<Int>>) {
+    var cursor_x = cursor_x_imported 
+    var cursor_y = cursor_y_imported
+    var visual_grid = visual_grid_imported
     val reader = InputStreamReader(System.`in`)
     loop@ while (true) {
         val ch = reader.read()
@@ -59,15 +134,39 @@ fun loop_main(){
                 val nextChar = reader.read()
                 if (nextChar == 91) {
                     when (reader.read()) {
-                        65 -> println("Up arrow pressed") // Up
-                        66 -> println("Down arrow pressed") // Down
-                        67 -> println("Right arrow pressed") // Right
-                        68 -> println("Left arrow pressed") // Left
+                        65 -> {
+                            cursor_x = if (cursor_x > 0) cursor_x - 1 else height - 1
+                            print_grid(visual_grid,height,width,cursor_x,cursor_y)
+                        }
+                        66 -> {
+                            cursor_x = if (cursor_x < height - 1) cursor_x + 1 else 0
+                            print_grid(visual_grid,height,width,cursor_x,cursor_y)
+                        }
+                        67 -> {
+                            cursor_y = if (cursor_y < width - 1) cursor_y + 1 else 0
+                            print_grid(visual_grid,height,width,cursor_x,cursor_y)    
+                        }
+                        68 -> {
+                            cursor_y = if (cursor_y > 0) cursor_y - 1 else width - 1
+                            print_grid(visual_grid,height,width,cursor_x,cursor_y)
+                        }
+                        else -> {
+                            println("Invalid arrow key input.")    
+                        }
                     }
                 }
             }
+            13 -> {
+                if (grid[cursor_x][cursor_y] == 1) {
+                    println("Game Over! You hit a mine!")
+                    break@loop
+                    } else {
+                        visual_grid[cursor_x][cursor_y] = get_adjacent_mines(cursor_x, cursor_y, grid,width,height)
+                        print_grid(visual_grid,height,width,cursor_x,cursor_y)
+                    }
+            }
             'q'.code, 'Q'.code -> {
-                println("Exiting Minesweeper CLI. Goodbye!")
+                println("Exiting Minesweeper CLI...")
                 break@loop
             }
             else -> {
@@ -75,4 +174,24 @@ fun loop_main(){
             }
         }
     }
+}
+
+
+fun get_adjacent_mines(x: Int, y: Int, grid: Array<Array<Int>>,width:Int,height:Int): Int {
+    var count = 0
+    for (i in -1..1) {
+        for (j in -1..1) {
+            if (i == 0 && j == 0) continue
+            val newX = x + i
+            val newY = y + j
+            if(newX < 0 || newX >= height || newY < 0 || newY >= width) continue
+            if (grid[newX][newY] == 1){
+                count += 1
+            }
+        }
+    }
+    if (count == 0){
+        count = 9
+    }
+    return count
 }
