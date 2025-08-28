5d2d1d2c69041b7ea1ba88458e46e17ea5b43c6f
Jimmy <150027133+Jimmxyz@users.noreply.github.com>
2025-08-28 18:12:33 +0200

    bug fix

--- main.kt
+++ main.kt
@@ -29,92 +29,64 @@ fun visual_grid_gen(rows: Int, cols: Int): Array<Array<Int>> {
     return Array(rows) { Array(cols) { 0 } }
 }
 
-fun print_grid(grid_to_display: Array<Array<Int>>,height: Int = 9, width: Int = 9, cursor_i: Int = 0, cursor_j: Int = 0) {
-    //Grid size 10 x 10 (test)
-    for (i in 0..height - 1) {
-        print("\u001b[1A")
+fun print_grid(grid_to_display: Array<Array<Int>>,height: Int = 9,width: Int = 9,cursor_i: Int = 0,cursor_j: Int = 0) {
+    repeat(height) {
+        print("\u001b[1A") 
+        print("\u001b[2K\r")
     }
+
     for (i in 0 until height) {
         print("\u001b[2K\r")
         for (j in 0 until width) {
-            //Bck color
-            var bck_color = "none"
-            if(grid_to_display[i][j] == 0 || grid_to_display[i][j] == 10) {
-                if((i % 2 == 0 && j % 2 == 0) || (i % 2 != 0 && j % 2 != 0)) {
-                    bck_color = "\u001b[48;2;88;168;88m"
-                } else {
-                    bck_color = "\u001b[48;2;82;153;82m"
-                }
+            val bck_color = if (grid_to_display[i][j] == 0 || grid_to_display[i][j] == 10) {
+                if ((i + j) % 2 == 0) "\u001b[48;2;88;168;88m" else "\u001b[48;2;82;153;82m"
             } else {
-                if((i % 2 == 0 && j % 2 == 0) || (i % 2 != 0 && j % 2 != 0)) {
-                    bck_color = "\u001b[48;2;231;164;133m"
-                } else {
-                    bck_color = "\u001b[48;2;214;140;105m"
+                if ((i + j) % 2 == 0) "\u001b[48;2;231;164;133m" else "\u001b[48;2;214;140;105m"
+            }
+
+            val font_color = when (grid_to_display[i][j]) {
+                1 -> "\u001b[38;2;73;74;210m"
+                2 -> "\u001b[38;2;64;120;67m"
+                3 -> "\u001b[38;2;210;73;73m"
+                4 -> "\u001b[38;2;140;57;161m"
+                5 -> "\u001b[38;2;243;237;38m"
+                6 -> "\u001b[38;2;106;232;226m"
+                7 -> "\u001b[38;2;133;133;131m"
+                10 -> "\u001b[38;2;210;0;0m"
+                else -> "\u001b[38;2;0;0;0m"
+            }
+
+            val cursor_color = "\u001b[38;2;255;255;255m"
+            if (i == cursor_i && j == cursor_j) {
+                val content = when (grid_to_display[i][j]) {
+                    0, 9 -> " "
+                    10 -> "F"
+                    else -> grid_to_display[i][j].toString()
                 }
-            }
-            // Cursor color
-            var cursor_color = "\u001b[38;2;255;255;255m"
-            
-            // Font color
-            var font_color = "none"
-            if (grid_to_display[i][j] == 1) {
-                font_color = "\u001b[38;2;73;74;210m"
-            } 
-            else if (grid_to_display[i][j] == 2) {
-                font_color = "\u001b[38;2;64;120;67m"
-            } 
-            else if (grid_to_display[i][j] == 3) {
-                font_color = "\u001b[38;2;210;73;73m"
-            } 
-            else if (grid_to_display[i][j] == 4) {
-                font_color = "\u001b[38;2;140;57;161m"
-            } 
-            else if (grid_to_display[i][j] == 5) {
-                font_color = "\u001b[38;2;243;237;38m"
-            } 
-            else if (grid_to_display[i][j] == 6) {
-                font_color = "\u001b[38;2;106;232;226m"
-            } 
-            else if (grid_to_display[i][j] == 7) {
-                font_color = "\u001b[38;2;133;133;131m"
-            } 
-            else if (grid_to_display[i][j] == 10) {
-                font_color = "\u001b[38;2;210;0;0m"
-            } 
-            else{
-                font_color = "\u001b[38;2;0;0;0m"
-            }
-            // Print the cell
-            if(i == cursor_i && j == cursor_j && grid_to_display[i][j] != 0 && grid_to_display[i][j] != 9 && grid_to_display[i][j] != 10) {
-                print("\u001B[1m"+ bck_color + cursor_color + "[" +  font_color  + grid_to_display[i][j] + "\u001b[0m" + cursor_color + bck_color + "\u001B[1m]\u001b[0m")
-            } else if(i == cursor_i && j == cursor_j && (grid_to_display[i][j] == 0 || grid_to_display[i][j] == 9)) {
-                print("\u001B[1m"+bck_color + cursor_color + "[ " + font_color + "\u001b[0m" + cursor_color + bck_color + "\u001B[1m]\u001b[0m")
-            } else if(i == cursor_i && j == cursor_j && grid_to_display[i][j] == 10) {
-                print("\u001B[1m"+bck_color + cursor_color + "[" + font_color + "F\u001b[0m" + cursor_color + bck_color + "\u001B[1m]\u001b[0m")
-            }
-            else if (grid_to_display[i][j] == 9 || grid_to_display[i][j] == 0) {
-                print("\u001B[1m"+bck_color + font_color + "   " + "\u001b[0m")
-            }
-            else if (grid_to_display[i][j] == 10) {
-                print("\u001B[1m"+bck_color + font_color + " F " + "\u001b[0m")
+                print("\u001B[1m$bck_color$cursor_color[$font_color$content$cursor_color$bck_color\u001B[1m]\u001b[0m")
             }
             else {
-                print("\u001B[1m"+bck_color + font_color + " " + grid_to_display[i][j] + " " + "\u001b[0m")
+                val content = when (grid_to_display[i][j]) {
+                    0, 9 -> "   "
+                    10 -> " F "
+                    else -> " ${grid_to_display[i][j]} "
+                }
+                print("\u001B[1m$bck_color$font_color$content\u001b[0m")
             }
         }
         println()
     }
+
+    // Dernière ligne d’instruction
     print("\u001b[2K\r")
     println("Press Q to quit, arrow to move, enter to select, and space to mark a mine.")
 }
 
+
 fun grid_readdy(height: Int, width: Int) {
     for (i in 0..height -1) {
         print("\u001b[2K\r")
-        for (j in 0..width - 1) {
-            print("---")
-        }
-        println()
+        println(i)
     }
 }
 
@@ -269,7 +241,9 @@ fun choose_size(): Pair<Int, Int> {
     var height_input = 10
     var height_cursor = 10
     print("\u001b[2K\r")
-    print("\u001b[48;2;255;255;255m\u001b[38;2;0;0;0m[" + ("-".repeat(width_cursor -5)) + "\u001b[0m" + (" ".repeat(46 - (width_cursor -5))) + " " + width_cursor + " " .repeat(45) + "]\u001b[0m")
+    println("\u001b[48;2;255;255;255m\u001b[38;2;0;0;0m[" + ("-".repeat(width_cursor -5)) + "\u001b[0m" + (" ".repeat(46 - (width_cursor -5))) + " " + width_cursor + " " .repeat(45) + "]\u001b[0m")
+    print("\u001b[2K\r")
+    print("Use <- and -> to adjust the width, press space to enter manually, and enter to confirm.")
     val reader = InputStreamReader(System.`in`)
     loop@ while (true) {
         val ch = reader.read()
@@ -306,6 +280,8 @@ fun choose_size(): Pair<Int, Int> {
                 break@loop
             }
         }
+        print("\u001b[2K\r")
+        print("\u001b[1A")
         if(width_cursor - 5 < 47) {
             var empty = ""
             if (width_cursor > 9){
@@ -335,8 +311,13 @@ fun choose_size(): Pair<Int, Int> {
             print("\u001b[2K\r")
             print("\u001b[48;2;255;255;255m\u001b[38;2;0;0;0m[" + ("-".repeat(46)) + "100" + "-".repeat(45) + "]\u001b[0m")
         }
+        println()
+        print("\u001b[2K\r")
+        print("Use <- and -> to adjust the width, press space to enter manually, and enter to confirm.")
     }
     if (width_input == -10) {
+        print("\u001b[2K\r")
+        print("\u001b[1A")
         print("\u001b[2K\r")
         print("\u001b[1A")
         print("\u001b[2K\r")
@@ -347,6 +328,8 @@ fun choose_size(): Pair<Int, Int> {
         print("\u001b[2K\r")
     }
     else{
+        print("\u001b[2K\r")
+        print("\u001b[1A")
         print("\u001b[2K\r")
         print("\u001b[1A")
         print("\u001b[2K\r")
@@ -362,7 +345,9 @@ fun choose_size(): Pair<Int, Int> {
             else{
                 empty = "  "
             }
-    print("\u001b[48;2;255;255;255m\u001b[38;2;0;0;0m[" + ("-".repeat(height_cursor -5)) + "\u001b[0m" + (" ".repeat(46 - (height_cursor -5))) + empty + height_cursor + " " .repeat(45) + "]\u001b[0m")
+    println("\u001b[48;2;255;255;255m\u001b[38;2;0;0;0m[" + ("-".repeat(height_cursor -5)) + "\u001b[0m" + (" ".repeat(46 - (height_cursor -5))) + empty + height_cursor + " " .repeat(45) + "]\u001b[0m")
+    print("\u001b[2K\r")
+    print("Use <- and -> to adjust the height, press space to enter manually, and enter to confirm.")
     loop@ while (true) {
         val ch = reader.read()
         if (ch == - 1) break
@@ -398,6 +383,8 @@ fun choose_size(): Pair<Int, Int> {
                 break@loop
             }
         }
+        print("\u001b[2K\r")
+        print("\u001b[1A")
         if(height_cursor - 5 < 47) {
             var empty = ""
             if (height_cursor > 9){
@@ -427,8 +414,13 @@ fun choose_size(): Pair<Int, Int> {
             print("\u001b[2K\r")
             print("\u001b[48;2;255;255;255m\u001b[38;2;0;0;0m[" + ("-".repeat(46)) + "100" + "-".repeat(45) + "]\u001b[0m")
         }
+        println()
+        print("\u001b[2K\r")
+        print("Use <- and -> to adjust the height, press space to enter manually, and enter to confirm.")
     }
     if (height_input == -10) {
+        print("\u001b[2K\r")
+        print("\u001b[1A")
         print("\u001b[2K\r")
         print("\u001b[1A")
         print("\u001b[2K\r")
@@ -442,9 +434,15 @@ fun choose_size(): Pair<Int, Int> {
         print("\u001b[2K\r")
         print("\u001b[1A")
         print("\u001b[2K\r")
+        print("\u001b[1A")
+        print("\u001b[2K\r")
         height_input = height_cursor
     }
     val width = width_input ?: 10
     val height = height_input ?: 10
     return Pair(width, height)
+}
+
+fun main_menu(){
+    // Future implementation for main menu
 }
